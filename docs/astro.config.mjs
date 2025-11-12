fe796e1d4d3074bce7c1a2c412b8cf4e21eb5d34
Andreas Kollegger <akollegger@gmail.com>
2025-11-12 13:57:33 +0000

    project: initialized an Astro project in docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Graph Annotation',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/neo4j-contrib/graph-annotation' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
